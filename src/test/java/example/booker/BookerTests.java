99c30aee4e89b08c59396eff2e6a16738f1b66e2
Andrey Brovko <aybrovko@avito.ru>
2024-02-01 20:13:36 +0300

    Booker api tests: deserialization

--- src/test/java/example/booker/BookerTests.java
+++ src/test/java/example/booker/BookerTests.java
@@ -2,6 +2,7 @@ package example.booker;
 
 import example.booker.pojo.Booking;
 import example.booker.pojo.BookingDates;
+import example.booker.pojo.BookingInfo;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,7 @@ public class BookerTests extends TestBase{
         BookingDates bookingDates = new BookingDates("2021-08-31", "2021-09-10");
         Booking booking = new Booking("John", "Doe", 100, true, bookingDates, "Breakfast");
 
+        //Серилизация
         given()
                 .contentType("application/json")
                 .accept("application/json")
@@ -22,5 +24,15 @@ public class BookerTests extends TestBase{
                 .post(Endpoints.booking)
                 .then()
                 .statusCode(200);
+
+        //Десерилизация
+        BookingInfo bookingInfo = given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(booking)
+                .when()
+                .post(Endpoints.booking)
+                .then()
+                .extract().body().as(BookingInfo.class);
     }
 }
---
+++ src/test/java/example/booker/pojo/BookingInfo.java
@@ -0,0 +1,18 @@
+package example.booker.pojo;
+
+public class BookingInfo {
+    private int bookingid;
+    private Booking booking;
+
+    public BookingInfo() {
+    }
+
+    public int getBookingid() {
+        return bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+}
